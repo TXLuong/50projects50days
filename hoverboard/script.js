ee9641e256f296acc0427008cf76ae33107444af
= <=>
2022-11-15 01:26:54 +0700

    try to save signature

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,16 +1,33 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 500
+const SQUARES = 1000
+const signature = [];
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
-    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseover', (e) => {
+        setColor(square);
+        signature.push(i);
+    })
 
     square.addEventListener('mouseout', () => removeColor(square))
 
     container.appendChild(square)
+}
+console.log(signature);
+while(signature.length > 0){
+    for(let i = 0; i < SQUARES; i++) {
+        console.log("why not print");
+        if(i in signature){
+            console.log(i);
+            const square = document.createElement('div')
+            square.classList.add('square')
+            setColor(square);
+            removeColor(square);
+        }
+    }
 }
 
 function setColor(element) {
